9093e3a2d42f1c474c0cdf0d574753a486cf25a5
Joachim <joso19am@student.cbs.dk>
2019-10-18 10:22:40 +0200

    billeder

--- LoginDatabase.js
+++ LoginDatabase.js
@@ -3,15 +3,6 @@ var userInfo = JSON.parse(localStorage.getItem("userList")) || [];
 // Making a function to register user and a variable for userList
 function registerUser() {
     var userList = [];
-
-    /* Making an if-statement to retrieve the register-form from local storage array
-    if (localStorage.getItem("User") == null) {
-        userList = []
-
-    } else {
-        userList = JSON.parse(localStorage.getItem("User"))
-    }
-    */
     var userInfo = {
         username: document.getElementById("username").value,
         password: document.getElementById("password").value,
