05ac27b8dc319737ac63e31e1ba276c30709eaf9
Rasmus Hother <raso19ab.student.cbs.dk>
2019-10-23 15:23:42 +0200

    Shoptøj

---
+++ EnrumSportingKurv.js
@@ -0,0 +1,18 @@
+var Name = "NewBullPadelCrandolBlue";
+document.getElementById("BullPadelCrandolPoloShirt")
+document.getElementById()
+
+console.log(document.getElementById
+
+document.getElementsByTagName(Herretøj)
+addProduct()
+
+var itemList = [];
+itemList.push(new Item("T-Shirt", "Crandol Blue","Size", "240,00"));
+
+var CrandolBlue;=
+
+class Item{
+    constructor(type,name,size,price){
+    }
+}
