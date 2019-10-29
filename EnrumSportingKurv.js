55dc0f03446776807a6a1ea64a1422595849af68
Joachim <joso19am@student.cbs.dk>
2019-10-29 11:08:20 +0100

    update to cart

--- EnrumSportingKurv.js
+++ EnrumSportingKurv.js
@@ -30,9 +30,9 @@ function addProduct() {
     }
 
     // Making a new product from the constructor in class "Product"
-    new Product("T-shirt", "Bullpadel Crandol Blue Polo Shirt", document.getElementById("Size1"), "240,00 DKK")
+    new Product("T-shirt", "Bullpadel Crandol Blue Polo Shirt", document.getElementById("Size1"), "240,00 DKK");
     // Pushing the new product to the array "productList"
-    productList.push(Product);
+    addToCart.onclick = productList.push(Product);
 
 
     // Saving the info pushed into the array "productList" in local storage
@@ -44,3 +44,18 @@ function addProduct() {
 
 
 }
+
+var showCart = document.getElementById("showCart");
+
+showCart.onclick = function (e) {
+    e.preventDefault();
+    showCart();
+
+// Function to show data in cart stored in array "productList"
+    function showCart() {
+        // Making a variable "currentCart" for the current info stored in local storage in the array "productList"
+        var currentCart = JSON.parse(localStorage.getItem("productList"));
+        console.log(currentCart);
+    }
+
+};
