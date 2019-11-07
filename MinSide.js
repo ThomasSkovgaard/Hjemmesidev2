675e6c81c171af049944e68a1733d47f9b0fb707
Joachim <joso19am@student.cbs.dk>
2019-11-07 11:56:39 +0100

    update NEW

--- MinSide.js
+++ MinSide.js
@@ -3,10 +3,11 @@
 window.onload = function checkIfLoggedIn() {
     if (localStorage.getItem('userList') == null) {
         window.location = 'SLogin.html'
+        alert ("Log ind først!")
     } else {
         window.location = 'S5.html'
     }
     return;
 }
 
-// OBS HVORFOR LOADER DEN FØRST HURTIGT MIN SIDE
+// OBS HVORFOR LOADER DEN FØRST HURTIGT MIN SIDE INDEN REDIRECT???
